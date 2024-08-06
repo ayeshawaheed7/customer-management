90c9a0be5aff412a4fe8b7ece16835d6de970b7b
ayeshawaheed7 <ayeshawaheed135@gmail.com>
2024-08-06 20:44:41 +0500

    add transactional annotation

--- customer/src/main/java/com/ayeshascode/customer/service/CustomerService.java
+++ customer/src/main/java/com/ayeshascode/customer/service/CustomerService.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Service
@@ -15,6 +16,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    @Transactional
     public void registerCustomer(String firstName, String lastName, String email) {
         if (isEmailAlreadyTaken(email)) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already taken");
