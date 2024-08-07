bd18a5747a359cbe990ae6af74e17a3eaa3b3fb0
ayeshawaheed7 <ayeshawaheed135@gmail.com>
2024-08-07 12:26:10 +0500

    add PostgresRunner import

--- customer/src/test/java/com/ayeshascode/customer/container/config/IntegrationTest.java
+++ customer/src/test/java/com/ayeshascode/customer/container/config/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.ayeshascode.customer.container.config;
 
 import com.ayeshascode.customer.CustomerApplication;
+import com.ayeshascode.customer.container.PostgresRunner;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
