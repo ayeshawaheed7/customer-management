620027a6786384d9df0c255b3d1cb24566858a0f
ayeshawaheed7 <ayeshawaheed135@gmail.com>
2024-08-13 19:43:36 +0500

    setup DB for notification service and PostgresRunner for integration test

--- notification/src/test/java/com/ayeshascode/notification/container/PostgresRunner.java
+++ notification/src/test/java/com/ayeshascode/notification/container/PostgresRunner.java
@@ -1,4 +1,4 @@
-package com.ayeshascode.fraud.container;
+package com.ayeshascode.notification.container;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
