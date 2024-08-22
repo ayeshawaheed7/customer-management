1894984437f72587cc4170f0884c72cf42354805
ayeshawaheed7 <ayeshawaheed135@gmail.com>
2024-08-22 17:58:43 +0500

    add kafka topic module

---
+++ kafka-topics/src/main/java/com/ayeshascode/KafkaTopicApplication.java
@@ -0,0 +1,11 @@
+package com.ayeshascode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTopicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTopicApplication.class, args);
+    }
+}
