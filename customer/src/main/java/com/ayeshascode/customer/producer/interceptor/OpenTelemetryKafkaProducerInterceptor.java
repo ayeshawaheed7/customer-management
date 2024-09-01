627e5b96a1e6051b19955af03a814bbdd4a0e2e1
ayeshawaheed7 <ayeshawaheed135@gmail.com>
2024-09-01 15:58:07 +0500

    clean code

--- customer/src/main/java/com/ayeshascode/customer/producer/interceptor/OpenTelemetryKafkaProducerInterceptor.java
+++ customer/src/main/java/com/ayeshascode/customer/producer/interceptor/OpenTelemetryKafkaProducerInterceptor.java
@@ -37,44 +37,6 @@ public class OpenTelemetryKafkaProducerInterceptor<K, V> implements ProducerInte
         return record;
     }
 
-    // Get the global tracer
-//    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("customer");
-
-//    @Override
-//    public ProducerRecord<K, V> onSend(ProducerRecord<K, V> record) {
-//        // Start a new span for the Kafka producer send operation
-//        Span span = tracer.spanBuilder("kafka.producer.send")
-//                .setSpanKind(SpanKind.PRODUCER)
-//                .startSpan();
-//
-//        log.info("create producer span: {}", span);
-//        try (Scope scope = span.makeCurrent()) {
-//            // Set the span as the current span
-//            SpanContext spanContext = span.getSpanContext();
-//
-//            if (spanContext.isValid()) {
-//                String traceparent = String.format("00-%s-%s-01",
-//                        spanContext.getTraceId(),
-//                        spanContext.getSpanId());
-//
-//                // Add traceparent to the Kafka headers
-//                Header traceparentHeader = new RecordHeader("traceparent", traceparent.getBytes());
-//                record.headers().add(traceparentHeader);
-//
-//                log.info("Added traceparent header: {}", traceparent);
-//            } else {
-//                log.warn("No valid span context found. Traceparent header not added.");
-//            }
-//
-//            return record;
-//        } finally {
-//            // End the producer span to capture the send operation
-//            span.end();
-//            log.info("End the producer span to capture the send operation: {}", span);
-//        }
-//    }
-
-
     @Override
     public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
         // Optionally handle acknowledgements
